8e3f2f7da8dc5e6090164a6ff4e7a0fb916079a0
Артём <79099792+Alfedov@users.noreply.github.com>
2022-07-24 01:01:59 +0300

    Добавьте файлы проекта.

---
+++ core/dllmain.cpp
@@ -0,0 +1,17 @@
+﻿#include <windows.h>
+#include "CChat.h"
+#include "kthook.hpp"
+
+using CHud__DrawRadar = void(__cdecl*)(); // Прототип CHud::DrawRadar
+
+kthook::kthook_simple_t<CHud__DrawRadar> samp_init_hook{ reinterpret_cast<void*>(0x58A330) }; // хук на CHud::DrawRadar
+
+void HOOK_after_samp_init() {
+    static bool inited = false; // переменная инициализации
+    namespace samp = sampapi::v037r2; // Ваша версия сампа, у меня это 037r3
+    if (!inited && samp::RefChat() != nullptr) { // если еще не инициализировались, и чат сампа уже инициализировался
+        samp::RefChat()->AddMessage(0xFFFFFFFF, "Этот текст выведен после инициализации сампа"); // Выводим сообщение
+        samp::RefChat()->m_debugColor = 0xFFFF0000; // Меняем цвет системных сообщений сампа на красный
+        inited = true; // отмечаем что иницализировались
+    }
+}
